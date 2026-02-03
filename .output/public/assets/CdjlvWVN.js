a3f5529ccf9d3875827137f60ae1a46107e89838
Kabalito <kabal.amaya@hotmailcom>
2026-02-03 08:33:06 -0400

    feat: add mock service worker and robots.txt; update build and deploy scripts
    - Added a new mock service worker file to handle API mocking during development.
    - Created a robots.txt file for search engine indexing control.
    - Updated the build script to simplify the build process.
    - Introduced a deploy script for generating static files and deploying to GitHub Pages.
    - Added gh-pages as a dependency for deployment.

---
+++ .output/public/assets/CdjlvWVN.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CoPD8Th-.js";import"./j5rfDhB1.js";import"./CKHXLV7k.js";import"./CmpRkaSt.js";import"./ep5wx385.js";import"./BHO9LvM_.js";import"./D3Wdx8Bl.js";export{o as default};
---
+++ .output/public/assets/DTeQ-7UM.js
@@ -0,0 +1,1 @@
+/* empty css        */import{c as r}from"./BHO9LvM_.js";const e=r("v-spacer","div","VSpacer");export{e as V};
